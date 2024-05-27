a139353b7ce9ed7aa958c6d7c7d081b5553223f2
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-05-27 19:20:37 -0300

    avatar image

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,14 +2,18 @@
 
 import Image from "next/image";
 
-const Avatar = () => {
+interface AvatarProps {
+  src: string | null | undefined;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ src }) => {
   return (
     <Image
-    className="rounded-full"
+      className="rounded-full"
       height={38}
       width={38}
       alt="Avatar"
-      src="/images/default-avatar.png"
+      src={src || "/images/default-avatar.png"}
     />
   );
 };
